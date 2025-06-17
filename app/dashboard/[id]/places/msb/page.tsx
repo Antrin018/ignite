9b038b303b3ba97129dc00ff7d678ceb8bdc88bb
Antrin018 <artyfowl00@gmail.com>
2025-06-18 03:20:28 +0530

    update 1.8

--- app/dashboard/[id]/places/msb/page.tsx
+++ app/dashboard/[id]/places/msb/page.tsx
@@ -6,7 +6,7 @@ export default function MainGate() {
     <DashboardLayout>
       <div
         className="h-full w-full bg-cover bg-center rounded-2xl flex justify-center items-start pt-16"
-        style={{ backgroundImage: "url('/images/msb.jpg')" }}>
+        style={{ backgroundImage: "url('/images/msb.jpeg')" }}>
          <div className="bg-black/60 rounded-2xl p-6 mx-6 text-white text-center text-3xl font-semibold max-w-3xl">
         This is MSB or mathematical science block: This is the maths and data science department of the college. It is newly established so images are not available.
         </div>

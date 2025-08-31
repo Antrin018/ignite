ba480661fdbbefe96f44620aa166db676b45d2d2
Antrin018 <artyfowl00@gmail.com>
2025-08-31 08:13:33 +0530

    Update page.tsx of msb with mv

--- app/dashboard/[id]/places/msb/page.tsx
+++ app/dashboard/[id]/places/msb/page.tsx
@@ -1,16 +1,13 @@
 'use client';
-import DashboardLayout from '@/components/DashboardLayout';
+import MobilePlaceLayout from '@/components/MobilePlaceLayout';
 
-export default function MainGate() {
+export default function MSB() {
   return (
-    <DashboardLayout>
-      <div
-        className="h-full w-full bg-cover bg-center rounded-2xl flex justify-center items-start pt-16"
-        style={{ backgroundImage: "url('/images/msb.jpeg')" }}>
-         <div className="bg-black/60 rounded-2xl p-6 mx-6 text-white text-center text-3xl font-semibold max-w-3xl">
-        This is MSB or mathematical science block: This is the maths and data science department of the college. It is newly established so images are not available.
-        </div>
-      </div>
-    </DashboardLayout>
+    <MobilePlaceLayout
+      backgroundImage="/images/msb.jpeg"
+      title="📍 MSB - Mathematical Science Block"
+    >
+      This is MSB or mathematical science block: This is the maths and data science department of the college. It is newly established.
+    </MobilePlaceLayout>
   );
 }

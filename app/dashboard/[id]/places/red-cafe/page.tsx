d21c5a79b7a5a8ab78f9099ba447290f51a3cad0
Antrin018 <artyfowl00@gmail.com>
2025-08-31 08:15:03 +0530

    Update page.tsx of red cafe wih mv

--- app/dashboard/[id]/places/red-cafe/page.tsx
+++ app/dashboard/[id]/places/red-cafe/page.tsx
@@ -1,23 +1,13 @@
 'use client';
-import DashboardLayout from '@/components/DashboardLayout';
+import MobilePlaceLayout from '@/components/MobilePlaceLayout';
 
-const handleTextClick=()=>{
-  window.location.href=''
-}
-
-export default function MainGate() {
+export default function RedCafe() {
   return (
-    <DashboardLayout>
-      <div
-        className="h-full w-full bg-cover bg-center rounded-2xl flex items-center justify-center items-start pt-16"
-        style={{ backgroundImage: "url('/images/red-cafe.jpeg')" }}>
-          <div 
-            className="bg-black/60 rounded-2xl p-6 mx-6 text-white text-center text-3xl font-semibold max-w-3xl cursor-pointer hover:bg-black/70 transition-all duration-300 animate-pulse"
-            onClick={handleTextClick}
-          >
-        This is Red Cafe: one of the resturants for having food and drinks. You can also order food from your hostels using their whatsapp
-        </div>
-      </div>
-    </DashboardLayout>
+    <MobilePlaceLayout
+      backgroundImage="/images/red-cafe.jpeg"
+      title="📍 Red Cafe"
+    >
+      This is Red Cafe: one of the restaurants for having food and drinks. You can also order food from your hostels using their whatsapp
+    </MobilePlaceLayout>
   );
 }

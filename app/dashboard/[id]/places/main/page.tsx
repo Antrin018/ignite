57ac4b2907d7be53f3daca85a399507c616e2f5d
Antrin018 <artyfowl00@gmail.com>
2025-08-31 08:11:35 +0530

    Update page.tsx of main gate wirth mv

--- app/dashboard/[id]/places/main/page.tsx
+++ app/dashboard/[id]/places/main/page.tsx
@@ -1,16 +1,13 @@
 'use client';
-import DashboardLayout from '@/components/DashboardLayout';
+import MobilePlaceLayout from '@/components/MobilePlaceLayout';
 
 export default function MainGate() {
   return (
-    <DashboardLayout>
-      <div
-        className="h-full w-full bg-cover bg-center rounded-2xl flex justify-center items-start pt-16"
-        style={{ backgroundImage: "url('/images/main.jpg')" }}>
-         <div className="bg-black/60 rounded-2xl p-6 mx-6 text-white text-center text-3xl font-semibold max-w-3xl">
-        This is the main gate of the campus that you see when entering.
-        </div>
-      </div>
-    </DashboardLayout>
+    <MobilePlaceLayout
+      backgroundImage="/images/main.jpg"
+      title="📍 Main Gate"
+    >
+      This is the main gate of the campus that you see when entering.
+    </MobilePlaceLayout>
   );
 }
